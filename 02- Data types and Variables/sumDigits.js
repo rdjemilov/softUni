d4746c61833cdf1af2491d5b35bbd5bfd7124350
Rasim Beitula <rasimbeitula@gmail.com>
2022-10-02 13:30:06 +0200

    added a comment

--- 02- Data types and Variables/sumDigits.js
+++ 02- Data types and Variables/sumDigits.js
@@ -1,7 +1,7 @@
 function sumDigits(number) {
     let numberAsStrings = number.toString();
     let sum = 0;
-    let numberOfDigits = numberAsStrings.length;
+    let numberOfDigits = numberAsStrings.length; //!!Това е важно! Извикването на .ленгт един път в променлива, вместо в цикъл.
     for (let i = 0; i < numberOfDigits; i++) {
         let currentDigit = Number(numberAsStrings[i]);
         sum += currentDigit;
