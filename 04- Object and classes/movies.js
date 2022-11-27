54e38b593e0e01e1e4d43ed89d58fdacc8781ddf
rdjemilov <rasimbeitula@gmail.com>
2022-11-27 14:09:29 +0100

    Fixed the issues about Movies task

--- 04- Object and classes/movies.js
+++ 04- Object and classes/movies.js
@@ -31,7 +31,32 @@
 function movies(moviesList) {
     let movies = [];
 
-    
+    moviesList.forEach(line => {
+        if(line.includes('addMovie ')) {
+            let name = line.split('addMovie ')[1];
+            movies.push({ name });
+        } else if(line.includes(' directedBy ')) {
+            let [name, director] = line.split(' directedBy ');
+            let movie = movies.find(el => el.name === name);
+
+            if(movie) {
+                movie.director = director;
+            }
+        } else if (line.includes(' onDate ')) {
+            let [name, date] = line.split(' onDate ');
+            let movie = movies.find(el => el.name === name);
+
+            if (movie) {
+                movie.date = date;
+            }
+        }
+    });
+
+    movies.forEach(movie => {
+        if (movie.name && movie.director && movie.date) {
+            console.log(JSON.stringify(movie));
+        }
+    })
 }
 
 movies([
