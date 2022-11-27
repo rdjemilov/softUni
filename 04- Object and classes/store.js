0dc2aa3d38e1b9c7d301758d120855121c331a0d
rdjemilov <rasimbeitula@gmail.com>
2022-11-27 23:34:38 +0100

    Exam Preparation tasks

--- 04- Object and classes/store.js
+++ 04- Object and classes/store.js
@@ -1,38 +1,59 @@
-function buildStore(available, delivery) {
-    const storedProducts = {}; // README: if don't need to change reference we should use const here
-    const availableLength = available.length;
-    const deliveryLength = delivery.length;
+// function buildStore(available, delivery) {
+//     const storedProducts = {}; // README: if don't need to change reference we should use const here
+//     const availableLength = available.length;
+//     const deliveryLength = delivery.length;
 
-    for (let index = 0; index < availableLength; index+= 2) {
-        const currentProduct = available[index];
-        storedProducts[currentProduct] = Number(available[index + 1 ]);
-    }
-    for (let index = 0; index < deliveryLength; index+= 2) {
-        const currentProduct = delivery[index];
+//     for (let index = 0; index < availableLength; index += 2) {
+//         const currentProduct = available[index];
+//         storedProducts[currentProduct] = Number(available[index + 1]);
+//     }
+//     for (let index = 0; index < deliveryLength; index += 2) {
+//         const currentProduct = delivery[index];
 
-        if (!storedProducts.hasOwnProperty(currentProduct)) {
-            storedProducts[currentProduct] = 0;
-        }
-        storedProducts[currentProduct] = Number(delivery[index + 1 ]);
+//         if (!storedProducts.hasOwnProperty(currentProduct)) {
+//             storedProducts[currentProduct] = 0;
+//         }
+//         storedProducts[currentProduct] = Number(delivery[index + 1]);
+//     }
+
+//     for (const product in storedProducts) {
+//         console.log(`${product} -> ${storedProducts[product]}`);
+//     }
+// }
+
+function buildStore(currentStocks, orderedStocks) {
+    const productsList = {};
+    let currentLength = currentStocks.length;
+    let orderedLength = orderedStocks.length;
+
+    for (let i = 0; i < currentLength; i += 2) {
+        let currentProduct = currentStocks[i];
+        productsList[currentProduct] = Number(currentStocks[i + 1]);
+    }
+    //FIXME: How can i increase the quantity for the given product
+    for (let i = 0; i < orderedLength; i += 2) {
+        let currentProduct = orderedStocks[i];
+        productsList[currentProduct] = Number(currentStocks[i + 1]);
+
+        
     }
 
-    for (const product in storedProducts) {
-        console.log(`${product} -> ${storedProducts[product]}`);
-    }
+
+    
 }
 
 buildStore([
 
     'Chips', '5', 'CocaCola', '9', 'Bananas',
-    
+
     '14', 'Pasta', '4', 'Beer', '2'
-    
-    ],
-    
+
+],
+
     [
-    
-    'Flour', '44', 'Oil', '12', 'Pasta', '7',
-    
-    'Tomatoes', '70', 'Bananas', '30'
-    
+
+        'Flour', '44', 'Oil', '12', 'Pasta', '7',
+
+        'Tomatoes', '70', 'Bananas', '30'
+
     ])
--- FinalExamPrep/01-activKeys.js
+++ FinalExamPrep/01-activKeys.js
@@ -8,44 +8,96 @@
  */
 //RESEARCH:
 
-function buildActivationKeys(input) {
-    let rawKey = input.shift();
-    let line = input.shift();
+// function buildActivationKeys(input) {
+//     let rawKey = input.shift();
+//     let line = input.shift();
 
-    while (line !== 'Generate') {
-        let tokens = line.split('>>>');
-        let command = tokens[0];
+//     while (line !== 'Generate') {
+//         let tokens = line.split('>>>');
+//         let command = tokens[0];
 
-        switch (command) {
-            case 'Contains': {
-                let subString = tokens[1];
+//         switch (command) {
+//             case 'Contains': {
+//                 let subString = tokens[1];
 
-                if (rawKey.includes(subString)) {
-                    console.log(`${rawKey} contains ${subString}`);
+//                 if (rawKey.includes(subString)) {
+//                     console.log(`${rawKey} contains ${subString}`);
+//                 } else {
+//                     console.log(`Substring not found!`);
+//                 }
+//                 break;
+//             }
+
+//             case 'Flip': {
+
+//             }
+//         }
+//     }
+
+// }
+
+function generateActivationKeys(input) {
+    // Input & Result data
+    let rawActivationKey = input.shift();
+    let instructions = input.slice();
+    let line = [];
+    let startIndex = [];
+    let endIndex = [];
+    let part = '';
+
+    // Loop through instructions
+    let instruction = instructions.shift();
+    while (instruction != 'Generate') {
+        // Apply instruction
+        let args = instruction.split('>>>');
+        let instructionName = args.shift();
+
+        switch (instructionName) {
+            case 'Contains':
+                let substring = args[0];
+                if (rawActivationKey.includes(substring)) {
+                    console.log(`${rawActivationKey} contains ${substring}`);
                 } else {
                     console.log(`Substring not found!`);
                 }
                 break;
-            }
+            case 'Flip':
+                line = args[0];
+                startIndex = args[1];
+                endIndex = args[2];
 
-            case 'Flip': {
+                part = rawActivationKey.substring(startIndex, endIndex);
+                let newPart = line == 'Upper' ? part.toUpperCase() : part.toLowerCase();
+                rawActivationKey = rawActivationKey.replace(part, newPart)
+                console.log(rawActivationKey);
+                break;
 
-            }
+            case 'Slice':
+                startIndex = args[0];
+                endIndex = args[1];
+                part = rawActivationKey.substring(startIndex, endIndex);
+                rawActivationKey = rawActivationKey.replace(part, '');
+                console.log(rawActivationKey);
+                break;
         }
+        // Get next isntruction
+        instruction = instructions.shift();
     }
 
+    // Print generated activation key
+    console.log(`Your activation key is: ${rawActivationKey}`);
 }
 
-buildActivationKeys([(["abcdefghijklmnopqrstuvwxyz",
+generateActivationKeys(["abcdefghijklmnopqrstuvwxyz",
 
-"Slice>>>2>>>6",
+    "Slice>>>2>>>6",
 
-"Flip>>>Upper>>>3>>>14",
+    "Flip>>>Upper>>>3>>>14",
 
-"Flip>>>Lower>>>5>>>7",
+    "Flip>>>Lower>>>5>>>7",
 
-"Contains>>>def",
+    "Contains>>>def",
 
-"Contains>>>deF",
+    "Contains>>>deF",
 
-"Generate"])])
+    "Generate"])
